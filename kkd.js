435fc9e7551f44806768cd64509f4b8d4318ad7d
Kamrin Hurley <128550647+KamH333@users.noreply.github.com>
2023-09-18 18:04:55 +0200

    Add files via upload

---
+++ kkd.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filterButtons = document.querySelectorAll("#filters button");
+    const projects = document.querySelectorAll(".project");
+
+    filterButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const filter = button.getAttribute("data-filter");
+            
+            projects.forEach((project) => {
+                if (filter === "all" || project.classList.contains(filter)) {
+                    project.style.display = "block";
+                } else {
+                    project.style.display = "none";
+                }
+            });
+        });
+    });
+});
